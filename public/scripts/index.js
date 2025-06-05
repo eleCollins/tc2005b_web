15c37c60497b5b0753972ccc4c5686a9571ad824
Collins <A01285888@tec.mx>
2025-06-05 10:49:43 -0600

    videogame build added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,13 +2,20 @@ const boton = document.getElementById("btnLogin");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-const login = () => {
-    // Validación de credenciales (usuario y contraseña fijos para ejemplo)
-    const correctUsername = "usuario";
-    const correctPassword = "contraseña";
-
-    if (username.value === correctUsername && password.value === correctPassword) {
-        sessionStorage.setItem("name", "Lucio Collins");  // Guardar nombre en sessionStorage
+const login = async () => {
+    // validar credenciales
+    const user = {username: username.value, password: password.value};
+    const data = await fetch("http://localhost:6001/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    });
+    const res = await data.json();
+    if(res.isLogin){
+        sessionStorage.setItem("name", res.name);  // Guardar nombre en sessionStorage
+        sessionStorage.setItem("id", res.user_id);  // Guardar id en sessionStorag
         window.location = "./pages/profile.html";  // Redirigir a perfil
     } else {
         alert("Credenciales incorrectas, por favor intente nuevamente.");
